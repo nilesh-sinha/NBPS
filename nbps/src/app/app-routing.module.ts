fa49b0b4e88c3f38273f8ae7f67a3e3b8e93994f
Nilesh Sinha <nileshinit@gmail.com>
2018-12-22 23:33:46 +0530

    dashboard page added

--- nbps/src/app/app-routing.module.ts
+++ nbps/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { LandingComponent } from './landing/landing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'dashboard', component: LandingComponent },
+  { path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- nbps/src/app/app.component.ts
+++ nbps/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nbps';
+
+  constructor(private _router: Router){
+
+  }
+
+  goHome(){
+    this._router.navigateByUrl('/dashboard');
+  }
 }
